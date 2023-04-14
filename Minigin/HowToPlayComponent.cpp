61e54f795a94b415a3f85a94a16b8574a214a022
Bram Vernimmen <vernimmenbram@gmail.com>
2023-04-14 22:01:48 +0200

    Added How To Play
    Added a way to show the user what buttons to use using ImGui.

---
+++ Minigin/HowToPlayComponent.cpp
@@ -0,0 +1,33 @@
+#include "HowToPlayComponent.h"
+#include "imgui.h"
+
+dae::HowToPlayComponent::HowToPlayComponent(std::weak_ptr<GameObject> pOwner)
+	: UpdateComponent(pOwner)
+{
+}
+
+void dae::HowToPlayComponent::DisplayGui()
+{
+	if (m_IsOpen)
+	{
+		ImGui::Begin("How To Play?", &m_IsOpen);
+
+		ImGui::Text("Player1:");
+		ImGui::Spacing();
+		ImGui::Text("Gain Score: Press 'u'");
+		ImGui::Text("Do Damage: Press 'j'");
+		ImGui::Dummy(ImVec2(0.0f, 25.0f));
+		ImGui::Text("Player2:");
+		ImGui::Spacing();
+		ImGui::Text("Gain Score: Press 'i'");
+		ImGui::Text("Do Damage: Press 'k'");
+		ImGui::Dummy(ImVec2(0.0f, 25.0f));
+		ImGui::Text("Please note; doing damage will give the same player extra points.");
+		ImGui::Text("When they die they get some more points.");
+
+		ImGui::End();
+	}
+	
+
+}
+
---
+++ Minigin/HowToPlayComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "UpdateComponent.h"
+
+namespace dae
+{
+
+	class HowToPlayComponent : public UpdateComponent
+	{
+	public:
+		HowToPlayComponent(std::weak_ptr<GameObject> pOwner);
+		virtual ~HowToPlayComponent() = default;
+		HowToPlayComponent(const HowToPlayComponent& other) = delete;
+		HowToPlayComponent(HowToPlayComponent&& other) = delete;
+		HowToPlayComponent& operator=(const HowToPlayComponent& other) = delete;
+		HowToPlayComponent& operator=(HowToPlayComponent&& other) = delete;
+
+		virtual void Update() override {};
+		virtual void DisplayGui() override;
+
+	protected:
+
+	private:
+		bool m_IsOpen{ true };
+	};
+
+}
